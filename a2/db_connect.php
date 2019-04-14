fea969a04c24796a46e2f949a3b6eea21a1ce6ba
doomSDey <sdptd20@gmail.com>
2019-04-14 16:19:49 +0530

    Update db_connect.php

--- a2/db_connect.php
+++ a2/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 	//make connection with database
-	$link = mysqli_connect("localhost","root","sudipta@2000","credit");
+	$link = mysqli_connect("localhost","root","***","credit");
 	// Check connection
 	if($link === false) {
     	die("ERROR: Could not connect. " . mysqli_connect_error());
